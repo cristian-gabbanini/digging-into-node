6533345d5c28e170bb578b1a3e0db46f168cb2f9
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-05-27 14:46:00 +0200

    Add ex7

--- exercises/ex7.js
+++ exercises/ex7.js
@@ -1,24 +1,50 @@
 #!/usr/bin/env node
+'use strict'
 
-"use strict";
-
-var util = require("util");
-// var childProc = require("child_process");
-
+var util = require('util')
+var childProc = require('child_process')
 
 // ************************************
 
-const HTTP_PORT = 8039;
-// const MAX_CHILDREN = 5;
+const HTTP_PORT = 8039
+const MAX_CHILDREN = 1500
 
-var delay = util.promisify(setTimeout);
+var delay = util.promisify(setTimeout)
 
-
-main().catch(console.error);
-
+main().catch(console.error)
 
 // ************************************
 
 async function main() {
-	// console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+    console.log(`Load testing http://localhost:${HTTP_PORT}...`)
+    while (true) {
+        let children = []
+        process.stdout.write(`Sending ${MAX_CHILDREN} requests... `)
+
+        for (let i = 0; i <= MAX_CHILDREN; i++) {
+            let child = newChildProcess()
+            children.push(child)
+        }
+
+        try {
+            await Promise.all(children)
+            console.log('Success!')
+            return
+        } catch (e) {
+            console.error('Something went wrong')
+        }
+
+        await delay(3000)
+    }
+}
+
+async function newChildProcess() {
+    return new Promise((res, rej) => {
+        let child = childProc.spawn('node', ['ex7-child.js'])
+        child.on('exit', function(code) {
+            if (code === 0) {
+                res(code)
+            } else rej(code)
+        })
+    })
 }
